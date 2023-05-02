cf213eaaf43355455594d3bd8d9f024994318e65
almendev <alfredo.jmp@icloud.com>
2023-05-02 01:14:57 +0100

    [FIX] hello.js file deleted

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -101,7 +101,6 @@ const Project = ({ type, title, img, link, github }: Props): JSX.Element => {
           className="w-full h-auto"
           whileHover={{ scale: 1.05 }}
           transition={{ duration: 0.2 }}
-          priority
           sizes="(max-width: 768px) 100vw,
       (max-width: 1200px) 50vw,
       50vw"
